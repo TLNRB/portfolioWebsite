5a8d246850c25c5587c64ec1633516f6492cb3d4
TLNRB <nortol01@easv365.dk>
2023-04-04 19:33:57 +0200

    gradient bg responsive done

--- portfolioWebsite/src/App.vue
+++ portfolioWebsite/src/App.vue
@@ -9,51 +9,47 @@
         Norbert<br />
         Tolnai
       </h1>
-      <div class="bg bg-1"></div>
-      <div class="bg bg-2"></div>
-      <div class="bg bg-3"></div>
     </div>
+    <div class="bg bg-1"></div>
+    <div class="bg bg-2"></div>
+    <div class="bg bg-3"></div>
   </section>
 </template>
 
 <style scoped>
 .home {
-  padding: 0.75rem;
   background-color: var(--bgNormalBlack);
 }
 
 .container {
   min-height: 100vh;
   padding: 0.75rem;
-  background-color: var(--bgBlack);
-  border-radius: 20px;
+  z-index: 5;
 }
-
+/* ----- Gradient Background ----- */
 .bg {
   position: fixed;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
+  margin: 0.75rem;
+  background-color: var(--bgBlack);
+  border-radius: 20px;
   animation: pulse 6s ease-in-out infinite alternate;
 }
 .bg-1 {
-  background-color: var(--bgBlack);
   background-image: radial-gradient(at 67% 64%, var(--bgBlack) 0, transparent 49%),
-    radial-gradient(at 95% 95%, var(--primaryColor) 0, transparent 30%);
+    radial-gradient(at 95% 130%, var(--primaryColor) 0, transparent 30%);
   z-index: 3;
   animation-delay: 0s;
 }
 .bg-2 {
-  background-color: var(--bgBlack);
-  background-image: radial-gradient(at 45% 110%, var(--primaryColor) 0, transparent 30%);
-  animation-delay: 4s;
-  z-index: 1;
+  display: none;
 }
 
 .bg-3 {
-  background-color: var(--bgBlack);
-  background-image: radial-gradient(at 5% 100%, var(--primaryColor) 0, transparent 30%);
+  background-image: radial-gradient(at 5% 130%, var(--primaryColor) 0, transparent 30%);
   animation-delay: 8s;
   z-index: 2;
 }
@@ -65,6 +61,25 @@
 
   100% {
     opacity: 0;
+  }
+}
+
+@media screen and (min-width: 768px) {
+  .bg-1 {
+    background-image: radial-gradient(at 67% 64%, var(--bgBlack) 0, transparent 49%),
+      radial-gradient(at 95% 120%, var(--primaryColor) 0, transparent 30%);
+  }
+  .bg-2 {
+    display: block;
+    background-image: radial-gradient(at 45% 120%, var(--primaryColor) 0, transparent 30%);
+    animation-delay: 4s;
+    z-index: 1;
+  }
+
+  .bg-3 {
+    background-image: radial-gradient(at 5% 120%, var(--primaryColor) 0, transparent 30%);
+    animation-delay: 8s;
+    z-index: 2;
   }
 }
 </style>
