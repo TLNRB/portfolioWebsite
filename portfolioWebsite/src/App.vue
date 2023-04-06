52467e9c7f70eac0f3dca304a53e3c6b2f5cd0fb
TLNRB <nortol01@easv365.dk>
2023-04-06 16:42:53 +0200

    about content added

--- portfolioWebsite/src/App.vue
+++ portfolioWebsite/src/App.vue
@@ -6,6 +6,33 @@ import Nav from './components/Nav.vue'
 <template>
   <Home />
   <Nav />
+  <section class="about">
+    <div class="title">
+      <h1><span>01</span>About.</h1>
+    </div>
+    <div class="content">
+      <p>
+        In Chronicle everything is made with Blocks that come with pixel perfect design,
+        interactivity and motion out of the box. Instead of designing from scratch, simply choose
+        the right one from our library of blocks and see the magic unfold.
+      </p>
+      <div class="btn-container">
+        <a class="btn" href=""
+          >Download CV
+          <font-awesome-icon :icon="['fas', 'download']" />
+        </a>
+        <a class="btn" href="">
+          Watch CV
+          <font-awesome-icon :icon="['fas', 'play']" />
+        </a>
+      </div>
+    </div>
+  </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+.about {
+  background-color: var(--bgNormalBlack);
+  padding: 6.25rem;
+}
+</style>
--- portfolioWebsite/src/main.js
+++ portfolioWebsite/src/main.js
@@ -7,7 +7,13 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import solid icons */
-import { faCirclePlay, faDownload, faCircleInfo, faXmark } from '@fortawesome/free-solid-svg-icons'
+import {
+  faCirclePlay,
+  faPlay,
+  faDownload,
+  faCircleInfo,
+  faXmark
+} from '@fortawesome/free-solid-svg-icons'
 /* import brand icons */
 import {
   faLinkedin,
@@ -21,6 +27,7 @@ import {
 /* add icons to the library */
 library.add(
   faCirclePlay,
+  faPlay,
   faDownload,
   faCircleInfo,
   faXmark,
