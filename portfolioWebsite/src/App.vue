0706e545d43e3f2aa752357add41cb3e11aebe5f
TLNRB <nortol01@easv365.dk>
2023-04-09 12:43:56 +0200

    projects design layout and responsive done

--- portfolioWebsite/src/App.vue
+++ portfolioWebsite/src/App.vue
@@ -47,6 +47,36 @@ import FooterSection from './components/FooterSection.vue'
               <button><font-awesome-icon :icon="['fas', 'circle-info']" /></button>
             </div>
           </div>
+
+          <div class="card">
+            <img src="./images/ref.jpg" alt="" />
+            <div class="lower-container">
+              <div class="info">
+                <h3>Pasta Perfect</h3>
+                <h3>Website</h3>
+              </div>
+              <div class="links">
+                <a href=""><font-awesome-icon :icon="['fab', 'github']" /></a>
+                <a href=""><font-awesome-icon :icon="['fas', 'circle-play']" /></a>
+              </div>
+              <button><font-awesome-icon :icon="['fas', 'circle-info']" /></button>
+            </div>
+          </div>
+
+          <div class="card">
+            <img src="./images/ref.jpg" alt="" />
+            <div class="lower-container">
+              <div class="info">
+                <h3>Pasta Perfect</h3>
+                <h3>Website</h3>
+              </div>
+              <div class="links">
+                <a href=""><font-awesome-icon :icon="['fab', 'github']" /></a>
+                <a href=""><font-awesome-icon :icon="['fas', 'circle-play']" /></a>
+              </div>
+              <button><font-awesome-icon :icon="['fas', 'circle-info']" /></button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -61,14 +91,14 @@ import FooterSection from './components/FooterSection.vue'
   background-color: var(--bgNormalBlack);
 }
 .container {
-  padding: 6.25rem;
+  padding: 6.25rem 5rem;
   background-color: var(--bgBlack);
   border-radius: 40px 40px 0 0;
 }
 
 .projects .container .title {
   margin-top: 0;
-  margin-left: -1.5rem;
+  margin-left: 0rem;
   display: flex;
   justify-content: start;
   align-items: center;
@@ -89,7 +119,7 @@ import FooterSection from './components/FooterSection.vue'
 }
 
 .projects .container .inner-container {
-  margin: 3.75rem 9rem;
+  margin: 3.75rem 9rem 0 9rem;
 }
 
 /* ----- Text and Filter Button -----*/
@@ -113,6 +143,7 @@ import FooterSection from './components/FooterSection.vue'
 }
 
 .projects .container .inner-container .text-container .filter-btn-container button {
+  font-family: 'Red Hat Display', sans-serif;
   padding: 0.813rem 1.5rem 0.75rem 1.5rem;
   background-color: transparent;
   border: 1px solid var(--textLightGray);
@@ -138,7 +169,7 @@ import FooterSection from './components/FooterSection.vue'
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  gap: 3.75rem;
+  gap: 3.25rem;
 }
 
 /* ----- Card -----*/
@@ -202,5 +233,203 @@ import FooterSection from './components/FooterSection.vue'
 
 .projects .container .inner-container .card-container .card .lower-container button:hover {
   color: var(--primaryColor);
+}
+
+@media screen and (max-width: 1400px) {
+  .projects .container .inner-container {
+    margin: 3.75rem 2rem 0 2rem;
+  }
+}
+
+@media screen and (max-width: 1068px) {
+  .container {
+    padding: 4rem;
+    padding-bottom: 6.25rem;
+  }
+  .projects .container .inner-container {
+    margin: 2.5rem 0;
+  }
+
+  .projects .container .title {
+    margin-left: -1.25rem;
+    gap: 1rem;
+  }
+
+  .projects .container .title h1 {
+    font-size: 7rem;
+  }
+
+  .projects .container .title .section-number {
+    border-bottom: 4px solid var(--primaryColor);
+    border-radius: 3px;
+  }
+
+  .projects .container .inner-container .text-container {
+    flex-direction: column;
+    align-items: start;
+    gap: 3.75rem;
+  }
+
+  .projects .container .inner-container .text-container p {
+    width: 90%;
+  }
+
+  .projects .container .inner-container .card-container {
+    margin-top: 2.5rem;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .container {
+    padding: 2rem;
+    padding-bottom: 6.25rem;
+    border-radius: 30px 30px 0 0;
+  }
+
+  .projects .container .inner-container .text-container p {
+    width: 100%;
+  }
+  .projects .container .inner-container .text-container .filter-btn-container button {
+    font-family: 'Red Hat Display', sans-serif;
+    padding: 0.688rem 1.25rem 0.625rem 1.25rem;
+    font-size: 0.875rem;
+  }
+}
+
+/* Title Resize */
+@media screen and (max-width: 660px) {
+  .projects .container .title {
+    margin-left: -0.75rem;
+  }
+
+  .projects .container .title h1 {
+    font-size: 4rem;
+  }
+
+  .projects .container .title .section-number {
+    letter-spacing: 0px;
+  }
+}
+
+@media screen and (max-width: 560px) {
+  .container {
+    padding: 1.5rem;
+    padding-bottom: 3.75rem;
+  }
+  .projects .container .inner-container .text-container p {
+    font-size: 1.125rem;
+  }
+
+  .projects .container .inner-container .card-container {
+    gap: 2.5rem;
+  }
+
+  /* Card Resize */
+  .projects .container .inner-container .card-container .card {
+    width: 300px;
+    height: 229px;
+  }
+
+  .projects .container .inner-container .card-container .card img {
+    height: 168px;
+  }
+
+  .projects .container .inner-container .card-container .card .lower-container {
+    padding: 0.625rem 1.5rem;
+  }
+
+  .projects .container .inner-container .card-container .card .lower-container .info h3 {
+    font-size: 1rem;
+    line-height: 1.2;
+  }
+
+  .projects .container .inner-container .card-container .card .lower-container .links {
+    margin: 0 1.25rem 0 auto;
+
+    gap: 0.625rem;
+  }
+
+  .projects .container .inner-container .card-container .card .lower-container .links a {
+    font-size: 1.375rem;
+    color: var(--textWhite);
+  }
+
+  .projects .container .inner-container .card-container .card .lower-container .links a:hover {
+    color: var(--primaryColor);
+  }
+
+  .projects .container .inner-container .card-container .card .lower-container button {
+    font-size: 1.375rem;
+  }
+}
+
+/* Title Resize */
+@media screen and (max-width: 400px) {
+  .container {
+    border-radius: 20px 20px 0 0;
+  }
+  .projects .container .inner-container {
+    margin: 2rem 0;
+  }
+
+  .projects .container .inner-container .text-container .filter-btn-container {
+    gap: 1rem;
+    flex-wrap: wrap;
+  }
+
+  .projects .container .title {
+    margin-left: -0.5rem;
+    gap: 0.75rem;
+  }
+
+  .projects .container .title h1 {
+    font-size: 2.5rem;
+  }
+
+  .projects .container .title .section-number {
+    border-bottom: 3px solid var(--primaryColor);
+  }
+}
+
+@media screen and (max-width: 360px) {
+  .projects .container .inner-container .card-container {
+    gap: 2rem;
+  }
+  /* Card Resize */
+  .projects .container .inner-container .card-container .card {
+    width: 250px;
+    height: 201px;
+  }
+
+  .projects .container .inner-container .card-container .card img {
+    height: 141px;
+  }
+
+  .projects .container .inner-container .card-container .card .lower-container {
+    padding: 0.625rem 1.125rem;
+  }
+
+  .projects .container .inner-container .card-container .card .lower-container .info h3 {
+    font-size: 0.875rem;
+    line-height: 1.3;
+  }
+
+  .projects .container .inner-container .card-container .card .lower-container .links {
+    margin: 0 0.625rem 0 auto;
+    gap: 0.625rem;
+  }
+
+  .projects .container .inner-container .card-container .card .lower-container .links a {
+    font-size: 1.25rem;
+    color: var(--textWhite);
+  }
+
+  .projects .container .inner-container .card-container .card .lower-container .links a:hover {
+    color: var(--primaryColor);
+  }
+
+  .projects .container .inner-container .card-container .card .lower-container button {
+    font-size: 1.25rem;
+  }
 }
 </style>
