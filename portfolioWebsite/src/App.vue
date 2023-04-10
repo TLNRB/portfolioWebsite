784c7872fd38fdcbafda508aa9f7dd84a8a649f4
TLNRB <nortol01@easv365.dk>
2023-04-10 11:26:39 +0200

    HomeView created, routing started

--- portfolioWebsite/src/App.vue
+++ portfolioWebsite/src/App.vue
@@ -1,22 +1,9 @@
 <script setup>
-import Home from './components/Home.vue'
-import Navbar from './components/Navbar.vue'
-import About from './components/About.vue'
-import Projects from './components/Projects.vue'
-import Skills from './components/Skills.vue'
-import FooterSection from './components/FooterSection.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <Navbar />
-  <Home :homeImg="'src/images/best.jpg'" />
-  <About />
-  <Projects />
-  <Skills
-    :skillsImg="'src/images/bubbleBG.png'"
-    :skillsImgMobile="'src/images/bubbleBGMobile.png'"
-  />
-  <FooterSection />
+  <RouterView />
 </template>
 
 <style scoped></style>
---
+++ portfolioWebsite/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Home from '../components/Home.vue'
+import Navbar from '../components/Navbar.vue'
+import About from '../components/About.vue'
+import Projects from '../components/Projects.vue'
+import Skills from '../components/Skills.vue'
+import FooterSection from '../components/FooterSection.vue'
+</script>
+
+<template>
+  <Navbar />
+  <Home :homeImg="'src/images/best.jpg'" />
+  <About />
+  <Projects />
+  <Skills
+    :skillsImg="'src/images/bubbleBG.png'"
+    :skillsImgMobile="'src/images/bubbleBGMobile.png'"
+  />
+  <FooterSection />
+</template>
+
+<style scoped></style>
