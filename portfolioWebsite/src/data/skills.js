46f772fd88df5c4cdcaeef07e9625c3f118ba476
TLNRB <nortol01@easv365.dk>
2023-04-16 13:53:13 +0200

    skills db created

---
+++ portfolioWebsite/src/data/skills.js
@@ -0,0 +1,77 @@
+//Image import on absolute path for build folder
+import refImg from '../assets/images/ref.jpg'
+
+const skills = [
+  {
+    id: 1,
+    title: 'Vue',
+    desc: 'Vue Icon',
+    img: refImg,
+    marginTop: 35
+  },
+  {
+    id: 2,
+    title: 'JavaScript',
+    desc: 'JS Icon',
+    img: refImg,
+    marginTop: 75
+  },
+  {
+    id: 3,
+    title: 'TailWind',
+    desc: 'TailWind Icon',
+    img: refImg,
+    marginTop: 15
+  },
+  {
+    id: 4,
+    title: 'GSAP',
+    desc: 'GSAP Icon',
+    img: refImg,
+    marginTop: 70
+  },
+  {
+    id: 5,
+    title: 'SCSS',
+    desc: 'SCSS Icon',
+    img: refImg,
+    marginTop: 100
+  },
+  {
+    id: 6,
+    title: 'Vue',
+    desc: 'Vue Icon',
+    img: refImg,
+    marginTop: 35
+  },
+  {
+    id: 7,
+    title: 'JavaScript',
+    desc: 'JS Icon',
+    img: refImg,
+    marginTop: 75
+  },
+  {
+    id: 8,
+    title: 'TailWind',
+    desc: 'TailWind Icon',
+    img: refImg,
+    marginTop: 15
+  },
+  {
+    id: 9,
+    title: 'GSAP',
+    desc: 'GSAP Icon',
+    img: refImg,
+    marginTop: 70
+  },
+  {
+    id: 10,
+    title: 'SCSS',
+    desc: 'SCSS Icon',
+    img: refImg,
+    marginTop: 100
+  }
+]
+
+export default skills
