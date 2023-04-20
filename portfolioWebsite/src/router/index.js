98bbe3fd002e7fe6ee2c9a440516426f344ec412
TLNRB <nortol01@easv365.dk>
2023-04-20 20:19:23 +0200

    404 page created

--- portfolioWebsite/src/router/index.js
+++ portfolioWebsite/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProjectView from '../views/ProjectView.vue'
+import NotFoundView from '../views/404View.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/project-:id',
       name: 'project',
       component: ProjectView
+    },
+    {
+      path: '/:catchall(.*)*',
+      name: 'notfound',
+      component: NotFoundView
     }
   ],
   scrollBehavior(to, from, savedPosition) {
---
+++ portfolioWebsite/src/views/404View.vue
@@ -0,0 +1,208 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <section class="container">
+    <div class="content">
+      <!-- Title -->
+      <div class="title">
+        <h1>4</h1>
+        <h1 id="brackets">{}</h1>
+        <h1>4</h1>
+      </div>
+
+      <p>Sorry, page not found!</p>
+      <RouterLink to="/" class="button">Back to Home</RouterLink>
+    </div>
+    <!-- Gradient Background -->
+    <div class="bg bg-1"></div>
+    <div class="bg bg-2"></div>
+    <div class="bg bg-3"></div>
+  </section>
+</template>
+
+<style scoped>
+.container {
+  background-color: var(--bgNormalBlack);
+}
+
+.container .content {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2.5rem;
+  position: relative;
+  padding: 11.58rem 4rem;
+  z-index: 5;
+}
+
+.container .content .title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.container .content h1 {
+  font-size: 15rem;
+  font-weight: bold;
+  color: var(--textLightGray);
+}
+
+.container .content #brackets {
+  transform: translateY(1.25rem);
+}
+
+.container .content p {
+  margin-top: -0.5rem;
+  font-size: 2rem;
+  color: var(--textNormalGray);
+  font-weight: 600;
+}
+
+.container .content .button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.75rem 1.5rem;
+  border: 1px solid var(--primaryColor);
+  background-color: var(--bgBlack);
+  color: var(--textLightGray);
+  border-radius: 60em;
+  font-size: 1rem;
+  font-weight: 600;
+
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
+  transition: all 0.3s linear;
+}
+.container .content .button::after {
+  content: '';
+  position: absolute;
+  bottom: -35px;
+  left: -25px;
+  height: 260px;
+  width: 230px;
+  border-radius: 38%;
+  box-sizing: border-box;
+  background-color: var(--primaryColor);
+  transform: translateY(0) rotate(0deg);
+  transition: all 1.25s linear;
+  z-index: -1;
+}
+.container .content .button:hover::after {
+  transform: translateY(-50%) rotate(90deg);
+}
+
+/* ----- Gradient Background ----- */
+.bg {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: 1.5rem;
+  background-color: var(--bgBlack);
+  border-radius: 20px;
+  animation: pulse 6s ease-in-out infinite alternate;
+}
+.bg-1 {
+  background-image: radial-gradient(at 67% 64%, var(--bgBlack) 0, transparent 49%),
+    radial-gradient(at 95% 120%, var(--primaryColor) 0, transparent 30%);
+  z-index: 3;
+  animation-delay: 0s;
+}
+.bg-2 {
+  background-image: radial-gradient(at 45% 120%, var(--primaryColor) 0, transparent 30%);
+  animation-delay: 4s;
+  z-index: 1;
+}
+
+.bg-3 {
+  background-image: radial-gradient(at 5% 120%, var(--primaryColor) 0, transparent 30%);
+  animation-delay: 8s;
+  z-index: 2;
+}
+
+@keyframes pulse {
+  0% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .container .content {
+    gap: 2rem;
+  }
+  .container .content h1 {
+    font-size: 10rem;
+  }
+
+  .container .content p {
+    margin-top: 0;
+    font-size: 1.5rem;
+  }
+
+  .container .content .button {
+    padding: 0.625rem 1.25rem;
+    font-size: 0.875rem;
+  }
+
+  /* ----- Gradient Background ----- */
+  .bg {
+    margin: 0.75rem;
+  }
+  .bg-1 {
+    background-image: radial-gradient(at 67% 64%, var(--bgBlack) 0, transparent 49%),
+      radial-gradient(at 95% 130%, var(--primaryColor) 0, transparent 30%);
+    z-index: 3;
+    animation-delay: 0s;
+  }
+  .bg-2 {
+    display: none;
+  }
+
+  .bg-3 {
+    background-image: radial-gradient(at 5% 130%, var(--primaryColor) 0, transparent 30%);
+    animation-delay: 8s;
+    z-index: 2;
+  }
+}
+
+@media screen and (max-width: 560px) {
+  .container .content {
+    padding: 11.58rem 2rem;
+  }
+  .container .content h1 {
+    font-size: 7rem;
+  }
+}
+
+@media screen and (max-width: 360px) {
+  .container .content {
+    padding: 11.58rem 1.5rem;
+  }
+
+  .container .content .title {
+    gap: 0.25rem;
+  }
+  .container .content h1 {
+    font-size: 5rem;
+  }
+  .container .content #brackets {
+    transform: translateY(0.75rem);
+  }
+
+  .container .content p {
+    font-size: 1.25rem;
+  }
+}
+</style>
