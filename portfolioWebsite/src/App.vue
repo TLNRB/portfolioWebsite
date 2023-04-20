27704bcf78d6453b10b458967b4f9edd10437138
TLNRB <nortol01@easv365.dk>
2023-04-20 16:17:09 +0200

    console log message added

--- portfolioWebsite/src/App.vue
+++ portfolioWebsite/src/App.vue
@@ -1,5 +1,9 @@
 <script setup>
 import { RouterView } from 'vue-router'
+
+console.log(
+  'Disable AddBlocker for displaying country code, weather temperature, current time, and for successful data fetch. '
+)
 </script>
 
 <template>
